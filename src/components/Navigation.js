34bc15e0c98234790f6cc58eda32ed3628888bf1
wensespl <wpenadillol@uni.pe>
2021-11-01 17:10:15 -0500

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,11 @@
 import { Navbar, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import useAuth from "../auth/useAuth";
+import roles from "../helpers/roles";
 import routes from "../helpers/routes";
 
 export default function Navigation() {
-  const { logout } = useAuth();
+  const { logout, user, isLogged } = useAuth();
   return (
     <Navbar collapseOnSelect expand="lg" variant="dark" bg="dark">
       <Navbar.Brand as={NavLink} to={routes.home}>
@@ -13,21 +14,41 @@ export default function Navigation() {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link as={NavLink} to={routes.admin.users}>
+          <Nav.Link
+            as={NavLink}
+            to={routes.admin.users}
+            style={{ display: user?.role === roles.admin ? "" : "none" }}
+          >
             Users
           </Nav.Link>
         </Nav>
         <Nav>
-          <Nav.Link as={NavLink} to={routes.login}>
+          <Nav.Link
+            as={NavLink}
+            to={routes.login}
+            style={{ display: isLogged() === false ? "" : "none" }}
+          >
             Iniciar Sesión
           </Nav.Link>
-          <Nav.Link as={NavLink} to={routes.register}>
+          <Nav.Link
+            as={NavLink}
+            to={routes.register}
+            style={{ display: isLogged() === false ? "" : "none" }}
+          >
             Registrarse
           </Nav.Link>
-          <Nav.Link as={NavLink} to={routes.courses}>
+          <Nav.Link
+            as={NavLink}
+            to={routes.courses}
+            style={{ display: isLogged() === true ? "" : "none" }}
+          >
             Mi cuenta
           </Nav.Link>
-          <Nav.Link to={routes.login} onClick={logout}>
+          <Nav.Link
+            to={routes.login}
+            onClick={logout}
+            style={{ display: isLogged() === true ? "" : "none" }}
+          >
             Cerrar Sesión
           </Nav.Link>
         </Nav>
