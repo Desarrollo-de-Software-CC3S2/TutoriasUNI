6f30549be7fc77b3cd2dc3a7b73422a2e2fb1c8e
wensespl <wpenadillol@uni.pe>
2021-11-21 19:59:15 -0500

    Update CourseItem.js
    Manejando el tipo de usuario

--- src/components/CourseItem.js
+++ src/components/CourseItem.js
@@ -1,21 +1,33 @@
 import { Card, Col, Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import routes from "../helpers/routes";
+import useAuth from "../auth/useAuth";
+import roles from "../helpers/roles";
 
 export default function CourseItem(props) {
+  const { user } = useAuth();
   return (
     <Col>
       <Card>
         <Card.Body>
           <Card.Title>{props.title}</Card.Title>
           <Card.Subtitle>Codigo</Card.Subtitle>
-          <Card.Text>
-            Tema: {props.tema} <br/>
+          <Card.Text
+            style={{ display: user?.role === roles.alumno ? "" : "none" }}
+          >
+            Tema: {props.tema} <br />
             Profesor: {props.tutor}
           </Card.Text>
-          <Button as={NavLink} to={routes.course(props.courseId)}>Ir a curso</Button>
+          <Card.Text
+            style={{ display: user?.role === roles.tutor ? "" : "none" }}
+          >
+            Tema: {props.tema}
+          </Card.Text>
+          <Button as={NavLink} to={routes.course(props.courseId)}>
+            Ir a curso
+          </Button>
         </Card.Body>
       </Card>
     </Col>
-  )
+  );
 }
