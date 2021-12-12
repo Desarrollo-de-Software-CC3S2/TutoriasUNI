6164fccf7146b0e9143929a8c6f40b89c5fa9029
wensespl <wpenadillol@uni.pe>
2021-12-11 23:54:56 -0500

    Update CoursePage.js
    Añadiendo la opción de subir documentos

--- src/pages/CoursePage.js
+++ src/pages/CoursePage.js
@@ -1,35 +1,132 @@
 import axios from "axios";
-import { Container, Accordion } from "react-bootstrap";
+import { Container, Accordion, Button, Modal, Form } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import useAuth from "../auth/useAuth";
+import ReactPlayer from "react-player/youtube";
+import { Formik, Form as Formk, Field } from "formik";
+import { useState } from "react";
+import roles from "../helpers/roles";
+import DocumentSchema from "../schemas/DocumentSchema";
 //import CourseData from "../assets/data/Cursos";
 
 export default function CoursePage() {
   const { courseId } = useParams();
-  const { course, setCourse } = useAuth();
+  const { course, setCourse, user } = useAuth();
+  const [modalShow, setModalShow] = useState(false);
+  var key = 0;
   //let CourseInfo = CourseData.filter((item) => item.id === Number(courseId));
   //console.log(CourseInfo[0].id);
-  const array = [0, 1, 2, 3, 4, 5, 6];
   return (
     <div style={{ display: "flex", margin: "2rem" }}>
       <Container>
         <h1>{course?.nombre}</h1>
+        <Button
+          variant="primary"
+          onClick={() => setModalShow(true)}
+          style={{ display: user?.rol === roles.tutor ? "" : "none" }}
+        >
+          Subir video
+        </Button>
         <Accordion defaultActiveKey="0" flush>
-          {array.map((item) => (
-            <Accordion.Item eventKey={String(item)}>
-              <Accordion.Header>Accordion Item #{item}</Accordion.Header>
+          {course?.contenido.map((item) => (
+            <Accordion.Item eventKey={key++}>
+              <Accordion.Header>{item.nombre}</Accordion.Header>
               <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                {item.tipo === "video" ? (
+                  <ReactPlayer controls url={item.link} />
+                ) : (
+                  ""
+                )}
               </Accordion.Body>
             </Accordion.Item>
           ))}
         </Accordion>
+        <Modal show={modalShow} onHide={() => setModalShow(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Subir Material</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Formik
+              initialValues={{
+                tipo: "",
+                link: "",
+              }}
+              validationSchema={DocumentSchema}
+              onSubmit={(values, { resetForm }) => {
+                var contenido = course?.contenido;
+                let valores = {
+                  tipo: values.tipo,
+                  link: values.link,
+                  nombre: values.nombre,
+                };
+                contenido.push(valores);
+                resetForm();
+                axios
+                  .patch(`http://localhost:8000/api/v1/courses/${courseId}`, {
+                    contenido: contenido,
+                  })
+                  .then((res) => {
+                    setCourse(res.data);
+                  })
+                  .catch((err) => console.log(err));
+                setModalShow(false);
+              }}
+            >
+              {({ errors, touched }) => (
+                <Form as={Formk}>
+                  <Form.Group className="mb-4">
+                    <Form.Label htmlForm="tipo">Tipo</Form.Label>
+                    <Field
+                      as={Form.Select}
+                      type="select"
+                      name="tipo"
+                      isInvalid={!!errors.tipo && touched.tipo}
+                      isValid={!errors.tipo && touched.tipo}
+                    >
+                      <option>Elija una opción</option>
+                      <option value="documento">Documento</option>
+                      <option value="video">Video</option>
+                    </Field>
+                    <Form.Control.Feedback type="invalid">
+                      {errors.tipo}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group className="mb-2">
+                    <Form.Label htmlFor="link">Nombre del material</Form.Label>
+                    <Form.Control
+                      as={Field}
+                      type="text"
+                      name="nombre"
+                      placeholder="Ingrese nombre del material"
+                      isInvalid={!!errors.nombre && touched.nombre}
+                      isValid={!errors.nombre && touched.nombre}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.nombre}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group className="mb-2">
+                    <Form.Label htmlFor="link">Enlace</Form.Label>
+                    <Form.Control
+                      as={Field}
+                      type="text"
+                      name="link"
+                      placeholder="Ingrese enlace del video"
+                      isInvalid={!!errors.link && touched.link}
+                      isValid={!errors.link && touched.link}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.link}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Subir
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </Modal.Body>
+        </Modal>
       </Container>
     </div>
   );
